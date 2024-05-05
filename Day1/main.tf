efb7370b97d0d1f5be94bf043b07c7d842aa3cd2
Sanjay shitole <scshitole@gmail.com>
2024-05-05 14:06:00 -0700

    Update main.tf

--- Day1/main.tf
+++ Day1/main.tf
@@ -13,18 +13,35 @@ terraform {
     }
   }
 }
+
 provider "bigip" {
   address  = "https://${var.address}:${var.port}"
   username = var.username
   password = var.password
 }
 
-resource "bigip_ltm_monitor" "monitor" {
-  name   = "/Common/terraform_monitor"
-  parent = "/Common/http"
+ resource "bigip_ltm_monitor" "monitor" {
+  name     = "/Common/terraform_monitor"
+  parent   = "/Common/http"
+  send     = "GET /some/path\r\n"
+  timeout  = "999"
+  interval = "999"
 }
+
 resource "bigip_ltm_pool" "pool" {
-  name                   = "/Common/Axiom_Environment_APP1_Pool"
-  load_balancing_mode    = "round-robin"
-  minimum_active_members = 1
-  monitors               = [bigip_ltm_monitor.monitor.name]
+  name                = "/Common/terraform-pool"
+  load_balancing_mode = "round-robin"
+  nodes               = ["10.0.0.171:80"]
+  monitors            = ["/Common/terraform_monitor"]
+  allow_snat          = "yes"
+  allow_nat           = "yes"
+}
+
+resource "bigip_ltm_virtual_server" "http" {
+  pool                       = "/Common/terraform-pool"
+  name                       = "/Common/terraform_vs_http"
+  destination                = "10.0.1.100"
+  port                       = 8080
+  source_address_translation = "automap"
+  depends_on                 = ["bigip_ltm_pool.pool"]
+}
